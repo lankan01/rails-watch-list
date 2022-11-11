cddb7f55baf4b472e6001ea561b2d7365233141e
Jega Pradeeba <pjega92@gmail.com>
2022-11-11 17:48:18 +0800

    Prepare rails app with external specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_11_083059) do
-  create_table "movies", force: :cascade do |t|
-    t.string "title", null: false
-    t.string "overview", null: false
-    t.string "poster_url", null: false
-    t.integer "rating", null: false
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_091136) do
+  create_table "bookmarks", force: :cascade do |t|
+    t.text "comment"
+    t.integer "movie_id"
+    t.integer "list_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_bookmarks_on_list_id"
+    t.index ["movie_id"], name: "index_bookmarks_on_movie_id"
+  end
+
+  create_table "lists", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  create_table "movies", force: :cascade do |t|
+    t.string "title"
+    t.string "overview"
+    t.string "poster_url"
+    t.integer "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookmarks", "lists"
+  add_foreign_key "bookmarks", "movies"
 end
