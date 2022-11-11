f04b7c15d1f61f3789e96495fdbc20ec5705bd44
Jega Pradeeba <pjega92@gmail.com>
2022-11-11 16:38:35 +0800

    movie app added model and seed of 4

--- Gemfile
+++ Gemfile
@@ -50,6 +50,8 @@ gem "bootsnap", require: false
 # Use Sass to process CSS
 # gem "sassc-rails"
 
+gem 'sqlite3', '~> 1.3', '>= 1.3.11'
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_11_083059) do
+  create_table "movies", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "overview", null: false
+    t.string "poster_url", null: false
+    t.integer "rating", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
